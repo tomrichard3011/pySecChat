d89c30c1512d18c1984668858d6e3890e93b4702
tomrichard3011 <richardtom52901@gmail.com>
2020-07-02 15:49:03 -0700

    updated ui

--- test_ui.py
+++ test_ui.py
@@ -1,5 +1,8 @@
-import curses, time
-from curses.textpad import Textbox, rectangle
+import curses
+import threading
+import time
+from curses.textpad import Textbox
+
 stdscr = curses.initscr()
 stdscr.keypad(True)
 
@@ -9,6 +12,22 @@ def enter_is_terminate(x):
         return 7
     else:
         return x
+
+
+def print_to_screen(output_win):
+    for i in range(10):
+        time.sleep(2)
+        # take note of where cursor is at
+        y, x = curses.getsyx()
+
+        # move cursor to location to write new message
+        output_win.addstr(("new message " + str(i) + "\n"))
+        output_win.refresh()
+
+        # place cursor back where it was.
+        curses.setsyx(y, x)
+        curses.doupdate()
+
 
 def draw_menu(stdscr):
     # set up output window boundaries
@@ -32,33 +51,37 @@ def draw_menu(stdscr):
     input_win = curses.newwin(input_inner_height, input_inner_width, input_uly - 1, input_ulx)
     output_win.border()
     input_win.border()
+
     # create window for text to appear
-    output_win_sub = output_win.derwin(output_inner_height - 1, output_inner_width - 2, 1, 1)
+    output_win_sub = output_win.derwin(output_inner_height - 2, output_inner_width - 2, 1, 1)
     input_win_sub = input_win.derwin((input_inner_height - 2), (input_inner_width - 2), 1, 1)
+
+    # set scrolling on output box
+    output_win_sub.scrollok(True)
+    output_win_sub.idlok(True)
+
+    # create Textbox object to allow input
     input_box = Textbox(input_win_sub)
 
+    # refresh screen to update all windows
     output_win.refresh()
     input_win.refresh()
-    input_win_sub.refresh()
 
     i = 0
+    new_thread = threading.Thread(target=print_to_screen, args=(output_win_sub,))
+    new_thread.start()
+
     while True:
         # set text box area
         # enter_is_terminate changes "enter" key to send message, instead of emacs key combo; ctrl-G
         input_box.edit(enter_is_terminate)
         # write message to output box
         message = input_box.gather()
-        output_win_sub.addstr(i, 0, message)
-
+        output_win_sub.addstr(message)
 
         # clear screen and refresh
         output_win_sub.refresh()
         input_win_sub.clear()
-
-        if i >= output_inner_height - 3:
-            i = 0
-        else:
-            i += 1
 
 
 curses.wrapper(draw_menu)
